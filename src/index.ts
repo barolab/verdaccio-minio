3cffb3736cc57ba626438303ab47534431fc24eb
barolab <rbailly17000@gmail.com>
2019-08-27 16:20:28 +0200

    Implement the required interface

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
-import { Logger, Config, PluginOptions } from '@verdaccio/types';
+import { Logger, Config, PluginOptions, IPluginStorage } from '@verdaccio/types';
 import { MinioConfig } from './config';
 import { Client } from 'minio';
 
+const VERSION = '0.1.0-alpha';
 const DEFAULT_BUCKET = 'verdaccio';
 const DEFAULT_REGION = 'us-east-1';
 
-export default class MinioDatabase {
+export default class MinioDatabase implements IPluginStorage<MinioConfig> {
   public logger: Logger;
   public config: MinioConfig;
+  public version: string;
   private client: Client;
   private bucket: string;
   private region: string;
 
   public constructor(config: Config, options: PluginOptions<MinioConfig>) {
+    this.version = VERSION;
     if (!config) {
       throw new Error('Minio storage is missing config. Add a `store.minio-storage` section to your config file');
     }
@@ -27,6 +30,46 @@ export default class MinioDatabase {
     this.initialize().catch(error => {
       throw new Error(`There was an error ensuring bucket "${this.bucket}" exist: ${error}`);
     });
+  }
+
+  public search(onPackage: Function, onEnd: Function, validate: Function): void {
+    throw new Error('Method not implemented.');
+  }
+
+  public get(callback: Function): void {
+    throw new Error('Method not implemented.');
+  }
+
+  public add(name: string, callback: Function): void {
+    throw new Error('Method not implemented.');
+  }
+
+  public remove(name: string, callback: Function): void {
+    throw new Error('Method not implemented.');
+  }
+
+  public getSecret(): Promise<string> {
+    throw new Error('Method not implemented.');
+  }
+
+  public setSecret(secret: string): Promise<any> {
+    throw new Error('Method not implemented.');
+  }
+
+  public readTokens(filter: import('@verdaccio/types').TokenFilter): Promise<import('@verdaccio/types').Token[]> {
+    throw new Error('Method not implemented.');
+  }
+
+  public saveToken(token: import('@verdaccio/types').Token): Promise<any> {
+    throw new Error('Method not implemented.');
+  }
+
+  public deleteToken(user: string, key: string): Promise<any> {
+    throw new Error('Method not implemented.');
+  }
+
+  public getPackageStorage(info: string): import('@verdaccio/types').IPackageStorage {
+    throw new Error('Method not implemented.');
   }
 
   private async initialize(): Promise<void> {
